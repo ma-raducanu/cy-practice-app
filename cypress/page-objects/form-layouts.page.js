6e00010b6d986c08aed98461d56ce85842b8a28f
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-30 18:55:21 +0300

    Completed the Methods Annotations lesson

--- cypress/page-objects/form-layouts.page.js
+++ cypress/page-objects/form-layouts.page.js
@@ -1,5 +1,11 @@
 class FormLayotsPage {
 
+  /**
+   * Method used to submit the "Using the Grid" form with valid user credentials
+   * @param {string} email valid user email
+   * @param {string} password valid user password
+   * @param {number} optionIndex index of the radio button, starting from 0
+   */
   submitUsingTheGridForm(email, password, optionIndex) {
     cy.contains('nb-card', 'Using the Grid').then(form => {
       cy.wrap(form).find('[placeholder="Email"]').type(email)
@@ -9,6 +15,12 @@ class FormLayotsPage {
     })
   }
 
+  /**
+   * Method used to submit the "Basic form" form with valid user credentials
+   * @param {string} email valid user email
+   * @param {string} password valid user password
+   * @param {boolean} isCheckboxSelected check/don't check the box, depending on boolean
+   */
   submitBasicForm(email, password, isCheckboxSelected) {
     cy.contains('nb-card', 'Basic form').then(form => {
       cy.wrap(form).find('[placeholder="Email"]').type(email)
