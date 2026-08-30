11022ba9e96ccf5df6320e1d0101cfc065a6d414
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-30 06:21:28 +0300

    Completed the Tooltips lesson

--- cypress/e2e/course/ui-components.cy.js
+++ cypress/e2e/course/ui-components.cy.js
@@ -7,7 +7,7 @@ beforeEach(() => {
 it('Input Fields', () => {
   cy.contains('Forms').click()
   cy.contains('Form Layouts').click()
-  cy.get('#inputEmail1').type('test@example.com', {delay: 200}).clear().type('hello').clear() // this will add a delay to cypress's typing speed, and after it has finished typing, it will clear the input field and enter the other text
+  cy.get('#inputEmail1').type('test@example.com', { delay: 200 }).clear().type('hello').clear() // this will add a delay to cypress's typing speed, and after it has finished typing, it will clear the input field and enter the other text
   const name = 'mircea'
   cy.contains('nb-card', 'Using the Grid').contains('Email').type(`${name}@test.com`)
   cy.get('#inputEmail1').should('have.value', `${name}@test.com`).clear().type('test@example.com').press(Cypress.Keyboard.Keys.TAB)
@@ -21,25 +21,25 @@ it('Radio Buttons', () => {
   cy.contains('Forms').click()
   cy.contains('Form Layouts').click()
   cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(allRadioButtons => {
-    cy.wrap(allRadioButtons).eq(0).check({force: true}).should('be.checked') // "eq" is index, and use force true only if no alternative is viable, as it will cause flakiness
-    cy.wrap(allRadioButtons).eq(1).check({force: true})
+    cy.wrap(allRadioButtons).eq(0).check({ force: true }).should('be.checked') // "eq" is index, and use force true only if no alternative is viable, as it will cause flakiness
+    cy.wrap(allRadioButtons).eq(1).check({ force: true })
     cy.wrap(allRadioButtons).eq(0).should('not.be.checked')
     cy.wrap(allRadioButtons).eq(2).should('be.disabled')
   })
-  cy.contains('nb-card', 'Using the Grid').contains('Option 1').click({force: true}) // you can check a radio button using its label, however, you will have to user click() instead of check()
-  cy.contains('nb-card', 'Using the Grid').contains('label', 'Option 1').find('input').check({force: true})
+  cy.contains('nb-card', 'Using the Grid').contains('Option 1').click({ force: true }) // you can check a radio button using its label, however, you will have to user click() instead of check()
+  cy.contains('nb-card', 'Using the Grid').contains('label', 'Option 1').find('input').check({ force: true })
 })
 
 it('Checkboxes', () => {
   cy.contains('Modal & Overlays').click()
   cy.contains('Toastr').click()
-  cy.get('[type="checkbox"]').check({force: true})
-  cy.get('[type="checkbox"]').should('be.checked') 
-  cy.get('[type="checkbox"]').click({force: true, multiple: true}) // check() will check all boxes when no specific box is targetted, and click() can be used to click multiple elements at the same time but needs the {multiple: true} argument
+  cy.get('[type="checkbox"]').check({ force: true })
+  cy.get('[type="checkbox"]').should('be.checked')
+  cy.get('[type="checkbox"]').click({ force: true, multiple: true }) // check() will check all boxes when no specific box is targetted, and click() can be used to click multiple elements at the same time but needs the {multiple: true} argument
   cy.get('[type="checkbox"]').should('not.be.checked')
 })
 
-it.only('Lists and Dropdowns', () => {
+it('Lists and Dropdowns', () => {
   cy.contains('Modal & Overlays').click()
   cy.contains('Toastr').click()
   cy.contains('div', 'Toast type:').find('select').select('info').should('have.value', 'info') // select is the native selector for dropdown fields, also, it's smart and it will select either the value attribute's value or the label
@@ -50,8 +50,15 @@ it.only('Lists and Dropdowns', () => {
     cy.wrap(dropdown).click()
     cy.get('.option-list nb-option').each((option, index, list) => { // each is a loop, and can contain multiple arguments
       cy.wrap(option).click()
-      if(index < list.length-1) // index starts with 0, but the list returns the actual number, which is 8, so at the end it will skip the final item as the 7 is not < 7
+      if (index < list.length - 1) // index starts with 0, but the list returns the actual number, which is 8, so at the end it will skip the final item as the 7 is not < 7
         cy.wrap(dropdown).click()
     })
   })
+})
+
+it.only('Tooltips', () => {
+  cy.contains('Modal & Overlays').click()
+  cy.contains('Tooltip').click()
+  cy.contains('button', 'Top').trigger('mouseenter') // inspect > "Elements" > "Event Listeners" and check all available events to determine which one is required as cypress does not support hover
+  cy.get('nb-tooltip').should('have.text', 'This is a tooltip')
 })
